09bc6b5092056bb2955fc850b30b896ecff0520d
Wimpy Katana <wim.katana@gmail.com>
2024-09-23 14:49:19 +0700

    addMedia

--- components/smallComponents/postItem.tsx
+++ components/smallComponents/postItem.tsx
@@ -1,10 +1,13 @@
-import { View, Text, Image } from 'react-native'
-import React, { memo } from 'react'
+import { View, Text, Image, Button } from 'react-native'
+import React, { memo, useEffect, useRef } from 'react'
+import { Video, ResizeMode } from 'expo-av'
 import { Post } from '../../types'
 import Hastag from './hastag'
 
 
 const PostItem = ({ item }: { item: Post }) => {
+    const videoRef = useRef<Video>(null);
+
     return (
         <View className='mt-1 py-4 mb-3 bg-gray-800'>
             <View className='flex flex-row px-3 mb-4'>
@@ -15,15 +18,32 @@ const PostItem = ({ item }: { item: Post }) => {
                 <Text className='text-white px-3 mb-1 text-2xl'>{item.userUsername}</Text>
             </View>
             <Text className='text-white px-3 text-xl mb-2'>{item.title}</Text>
-            <Image
-                className='w-full'
-                style={{ aspectRatio: item.mediaWidth / item.mediaHeight }} 
-                source={
-                    item.mediaThumbnail ? 
-                        { uri: `https://cache.lahelu.com/${item.mediaThumbnail}` } 
-                    : 
-                        {uri: `https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg`}} 
-            />
+            {
+            item.mediaType === 0 ?
+
+                <Image
+                    className='w-full'
+                    style={{ aspectRatio: item.mediaWidth / item.mediaHeight }} 
+                    source={
+                        item.media ? 
+                            { uri: `https://cache.lahelu.com/${item.media}` } 
+                        : 
+                            {uri: `https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg`}} 
+                />
+            :
+                <View>
+                    <Video
+                        ref={videoRef}
+                        className='w-full'
+                        source={{ uri: `https://cache.lahelu.com/${item.media}` }}
+                        useNativeControls
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        style={{ aspectRatio: item.mediaWidth / item.mediaHeight }}
+                    />
+                </View>
+            }
+            
             <Hastag data={item.hashtags} />
         </View>
     )
